2e45366202735646acdd9592c726c1c65176b37b
amirdaeiFA <amirdaei@gmail.com>
2020-04-10 18:03:00 +0430

    first

---
+++ js/app.js
@@ -0,0 +1,16 @@
+const email = document.getElementById("input");
+const error = document.getElementById("error");
+const form = document.getElementById("form");
+
+form.addEventListener("submit", emailValidator);
+function emailValidator(e) {
+	e.preventDefault();
+	let filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+	if (email.value === "") {
+		error.textContent = `You must enter an email address!`;
+	} else if (!filter.test(email.value)) {
+		error.textContent = `Please provide a valid email address`;
+		email.focus;
+		return false;
+	}
+}

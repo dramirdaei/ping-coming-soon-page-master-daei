4564b3fec368c44d8ed0b015ed42313d9bc8c1a2
amirdaeiFA <amirdaei@gmail.com>
2020-04-10 18:53:37 +0430

    second

--- js/app.js
+++ js/app.js
@@ -1,15 +1,16 @@
 const email = document.getElementById("input");
 const error = document.getElementById("error");
 const form = document.getElementById("form");
-
 form.addEventListener("submit", emailValidator);
 function emailValidator(e) {
 	e.preventDefault();
 	let filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
 	if (email.value === "") {
 		error.textContent = `You must enter an email address!`;
+		email.classList.add("error2");
 	} else if (!filter.test(email.value)) {
 		error.textContent = `Please provide a valid email address`;
+		email.classList.add("error2");
 		email.focus;
 		return false;
 	}
